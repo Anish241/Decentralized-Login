ef3f278688b25af65bcefb12c66d9abe5e3e9188
Anish <aniish9.18@gmail.com>
2023-04-25 21:27:48 +0530

    hashing

--- smart_contracts/contracts/User.sol
+++ smart_contracts/contracts/User.sol
@@ -3,57 +3,46 @@ pragma solidity ^0.8.0;
 
 contract User {
 
-    uint256 userCount;
-    
-    struct UserData {
-        string username;
-        string password;
-        bool registered;
+uint256 userCount;
+
+struct UserData {
+    string username;
+    bytes32 passwordHash; // store password as a hashed value
+    bool registered;
+}
+
+
+UserData [] public users;
+
+//register a user
+function registerUser(string memory _username, string memory _password) public returns (bool) {
+    // check if user already exists
+    for (uint256 i = 0; i < users.length; i++) {
+        if (keccak256(abi.encodePacked(users[i].username)) == keccak256(abi.encodePacked(_username))) {
+            return false;
+        }
     }
-    
-    
-   //make an array of users
+    bytes32 passwordHash = keccak256(abi.encodePacked(_password)); // hash the password
+    users.push(UserData(_username, passwordHash, true));
+    userCount++;
+    return true;
+}
 
-
-   UserData [] public users;
-
-   //register a user
-
-    function registerUser(string memory _username, string memory _password) public returns (bool) {
-         // check if user already exists
-            for (uint256 i = 0; i < users.length; i++) {
-                if (keccak256(abi.encodePacked(users[i].username)) == keccak256(abi.encodePacked(_username))) {
-                    return false;
-                }
-            }
-         users.push(UserData(_username, _password, true));
-         userCount++;
-         return true;
-    }
-
-    //login a user
-
-    function loginUser(string memory _username, string memory _password) public view returns (bool) {
-        for (uint256 i = 0; i < users.length; i++) {
-            if (keccak256(abi.encodePacked(users[i].username)) == keccak256(abi.encodePacked(_username))) {
-                if (keccak256(abi.encodePacked(users[i].password)) == keccak256(abi.encodePacked(_password))) {
-                    return true;
-                }
+//login a user
+function loginUser(string memory _username, string memory _password) public view returns (bool) {
+    bytes32 passwordHash = keccak256(abi.encodePacked(_password)); // hash the input password
+    for (uint256 i = 0; i < users.length; i++) {
+        if (keccak256(abi.encodePacked(users[i].username)) == keccak256(abi.encodePacked(_username))) {
+            if (users[i].passwordHash == passwordHash) { // compare hashed password
+                return true;
             }
         }
-        return false;
     }
+    return false;
+}
 
-
-    //get user count
-
-    function getUserCount() public view returns (uint256) {
-        return userCount;
-    }
-
-
-
-
-
-
+//get user count
+function getUserCount() public view returns (uint256) {
+    return userCount;
+}
 }
